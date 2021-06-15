70a5fc1154e6c4df98ce9bf74aed5dc4a5ad85a9
Laia Lloret <laialloret95@gmail.com>
2021-06-15 11:13:48 +0200

    remove unnedeed constructor

--- src/components/SingleColorPicker/SingleColorPicker.js
+++ src/components/SingleColorPicker/SingleColorPicker.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 
 class SingleColorPicker extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            index: 0,
-        }
-    }
-    
+
     render() {
         const { color, value, onChange } = this.props;
         let BGcolor = color === 'R' ? "red" 
